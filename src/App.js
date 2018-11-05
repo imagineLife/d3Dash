aa02deab6bebee459b4603bbb58b5fd03f6d114d
Eric Laursen <ericjlaursen@gmail.com>
2018-11-05 16:39:37 -0500

    adds dummy navLinks to state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Nav from './components/Nav'
-const App = () => {
-  return (
-    <main>
-    	<Nav />
-    </main>
-  );
+class App extends React.Component{
+	constructor(props){
+		super(props)
+
+		this.state = {
+			links : [
+				{
+					txt: 'test1',
+					url: 'url1'
+				},
+				{
+					txt: 'test2',
+					url: 'url2'
+				},
+				{
+					txt: 'test3',
+					url: 'url3'
+				},
+				{
+					txt: 'test4',
+					url: 'url4'
+				}
+			]
+		}
+	}
+
+	render(){
+		return (
+			<main>
+				<Nav navLinks={this.state.links}/>
+			</main>
+		);
+	}
 };
 export default App;
