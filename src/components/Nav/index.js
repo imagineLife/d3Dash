ef75cedf5a85fa1c107467221dc84a04dcc01660
Eric Laursen <ericjlaursen@gmail.com>
2018-11-09 08:30:25 -0500

    converts from class to fn

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,68 +2,41 @@ import React from 'react';
 import './index.css';
 import './hamburger.css';
 
-export default class Nav extends React.Component {
-	constructor(props){
-		super(props)
+export default function Nav(props){
 
-		this.state = {
-			toggle : 'closed',
-			navVisible: false
-		}
+	let newToggle = (props.openState == true) ? 'open' : 'closed'
 
-		this.toggleNav = this.toggleNav.bind(this)
-		this.changeSandwich = this.changeSandwich.bind(this)
+	//array of Objects,
+	//these are properties of each NavLink below	
+	const navLinkArray = [
+		{
+			linkTo : "/about",
+			imgSrc : "/imgs/info.ico",
+			alt : "About"
+		},
+	];
+
+	let toggleClass = (props.openState == true) ? 'is-active' : '';
+	let sandClass = `hamburger hb-simple ${toggleClass}`
+
+	let sandwichIcon = <button className={sandClass} type="button" onClick={props.changeNavState}>
+		  <span className="hamburger-box">
+		    <span className="hamburger-inner"></span>
+		  </span>
+		</button>;
+	const navStyle = {
+		width: (props.openState == true) ? '200px' : '0px',
+
 	}
 
-	toggleNav(){
-		console.log('togglingNav')
-		this.changeSandwich()
-		this.setState({navVisible: (this.state.navVisible == true) ? false : true})
-	}
+	let sideNav = <nav id="mySidenav" className="sidenav" style={navStyle}>
+		<p>NavMicCheck</p>
+	</nav>;
 
-	//Help update this specific navLink text to change when signed-in-or-not :) 
-	changeSandwich(){
-		console.log('updating sandwich')
-		let curTogState = this.state.toggle;
-		let newToggle = (curTogState == 'closed') ? 'open' : 'closed'
-		this.setState({toggle: newToggle})
-	}
-
-	render(){
-		console.log('rendering')
-		console.log(this.state.toggle)
-		//array of Objects,
-		//these are properties of each NavLink below	
-		const navLinkArray = [
-			{
-				linkTo : "/about",
-				imgSrc : "/imgs/info.ico",
-				alt : "About"
-			},
-		];
-
-		let toggleClass = (this.state.navVisible == true) ? 'is-active' : '';
-		let sandClass = `hamburger hb-simple ${toggleClass}`
-
-		let sandwichIcon = <button className={sandClass} type="button" onClick={this.toggleNav}>
-			  <span className="hamburger-box">
-			    <span className="hamburger-inner"></span>
-			  </span>
-			</button>;
-		const navStyle = {
-			width: (this.state.navVisible == true) ? '200px' : '0px',
-
-		}
-
-		let sideNav = <nav id="mySidenav" className="sidenav" style={navStyle}>
-			<p>NavMicCheck</p>
-		</nav>;
-
-		return (
-			<React.Fragment>
-			{sideNav}
-			{sandwichIcon}
-			</React.Fragment>
-	    );
-	}
+	return (
+		<React.Fragment>
+		{sideNav}
+		{sandwichIcon}
+		</React.Fragment>
+    );
 }
