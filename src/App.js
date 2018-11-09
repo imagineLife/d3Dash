ad4c95bedee2c6a6ab6674dc5e86e3939dce5752
Eric Laursen <ericjlaursen@gmail.com>
2018-11-09 08:30:05 -0500

    passes changeNavState prop to nav

--- src/App.js
+++ src/App.js
@@ -33,11 +33,7 @@ class App extends React.Component{
 	}
 
 	closeNavIfOpen(){
-		console.log('navOpen?!')
-		console.log(this.state.navOpen)
-		if(this.state.navOpen == true){
-			this.setState({navOpen: false})
-		}
+		this.setState({navOpen: !this.state.navOpen})
 	}
 
 	render(){
@@ -46,6 +42,7 @@ class App extends React.Component{
 				<Nav 
 					navLinks={this.state.links} 
 					openState={this.state.navOpen}
+					changeNavState={this.closeNavIfOpen}
 				/>
 			</main>
 		);
