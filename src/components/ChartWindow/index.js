a18980c160b979a932e04e3e9ea0d536dc11fa06
Eric Laursen <ericjlaursen@gmail.com>
2018-11-09 13:45:20 -0500

    Includes chartWindow Component

---
+++ src/components/ChartWindow/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+import './hamburger.css';
+
+export default function ChartWindow(props){
+	return (
+		<iframe 
+			className='myFrame'
+			src='../../pages/bar'
+		/>
+    );
+}
