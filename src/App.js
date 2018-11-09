62b403375c7a21bf91c69cf6dbaec0712efc4f1a
Eric Laursen <ericjlaursen@gmail.com>
2018-11-09 13:45:13 -0500

    Includes chartWindow Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Nav from './components/Nav'
 import './resetNormalize.css'
 import './main.css'
+import Nav from './components/Nav'
+import ChartWindow from './components/ChartWindow'
 class App extends React.Component{
 	constructor(props){
 		super(props)
@@ -32,18 +33,23 @@ class App extends React.Component{
 		this.closeNavIfOpen = this.closeNavIfOpen.bind(this)
 	}
 
-	closeNavIfOpen(){
-		this.setState({navOpen: !this.state.navOpen})
+	closeNavIfOpen(e){
+		console.log('e')
+		console.log(e.target)
+		// if(this.state.navOpen == true){
+			this.setState({navOpen: !this.state.navOpen})
+		// }
 	}
 
 	render(){
 		return (
-			<main onClick={this.closeNavIfOpen}>
+			<main onClick={e => this.closeNavIfOpen}>
 				<Nav 
 					navLinks={this.state.links} 
 					openState={this.state.navOpen}
 					changeNavState={this.closeNavIfOpen}
 				/>
+				<ChartWindow changeNavState={this.closeNavIfOpen}/>
 			</main>
 		);
 	}
