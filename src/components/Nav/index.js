5ff8dfdca23077b121fe5c0044c94b561307bd83
Eric Laursen <ericjlaursen@gmail.com>
2019-01-22 19:19:07 -0500

    destructures props in function params

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './index.css';
 import './hamburger.css';
 
-export default function Nav(props){
+export default function Nav({openState, navLinks, changeNavState, toggleNavBar}){
 	// console.log('nav props.navLinks')
 	// console.log(props.navLinks)
-	let newToggle = (props.openState == true) ? 'open' : 'closed'
+	let newToggle = (openState == true) ? 'open' : 'closed';
 
-	const navLinks = props.navLinks.map(l => {
+	const theseNavLinks = navLinks.map(l => {
 
 		let linkChildren = l.children.map(ch => {
 			return <li key={ch} className="linkChild">{ch}</li>
@@ -18,24 +18,24 @@ export default function Nav(props){
 		return <li 
 			key={l.parent} 
 			value={l.parent}
-			onClick={props.changeNavState}>
+			onClick={changeNavState}>
 				{l.parent}
 			</li>
 	});
 
-	let toggleClass = (props.openState == true) ? 'is-active' : '';
+	let toggleClass = (openState == true) ? 'is-active' : '';
 	let sandClass = `hamburger hb-simple ${toggleClass}`
 
 	let sandwichIcon = <button 
 		className={sandClass} 
 		type="button" 
-		onClick={props.toggleNavBar}>
+		onClick={toggleNavBar}>
 		  <span className="hamburger-box">
 		    <span className="hamburger-inner"></span>
 		  </span>
 		</button>;
 	const navStyle = {
-		width: (props.openState == true) ? '125px' : '0px',
+		width: (openState == true) ? '125px' : '0px',
 
 	}
 
@@ -44,9 +44,9 @@ export default function Nav(props){
 		className="sidenav" 
 		style={navStyle}>
 			<ul>
-				{navLinks}
+				{theseNavLinks}
 			</ul>
-			<li className="closeLink linkChild" onClick={props.toggleNavBar}>CLOSE</li>
+			<li className="closeLink linkChild" onClick={toggleNavBar}>CLOSE</li>
 		</nav>;
 
 	return (
